0c865d8143f61011592b2d8c8d9514ac031ae3e5
Aria Myers (RIT Student) <jsm3767@ad.rit.edu>
2018-04-09 10:27:15 -0400

    Reorganized Files

---
+++ Mete/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    float songBPM;
+    float secondsPerBeat;
+
+    GameObject Chunk;
+
+    float timer;
+    private int shake;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        songBPM = 60;
+        secondsPerBeat = 60.0f / songBPM;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if(timer > secondsPerBeat)
+        {
+            timer -= secondsPerBeat;
+            Pulse();
+        }
+	}
+
+    void Pulse()
+    {
+        Debug.Log("Boom");
+    }
+}
