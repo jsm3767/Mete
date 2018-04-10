8c15093b6d03e14b2c6e73416d5cfe23b364d320
Paul Wilson <pjw6900@rit.edu>
2018-04-10 12:52:28 -0400

    Start of Loading Chunks

---
+++ Mete/Assets/ChunkLoading.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChunkLoading : MonoBehaviour {
+
+    public static ChunkLoading Instance;
+    public static SceneManager SceneManagerInstance;
+    //[SerializeField] GameObject ChunkOne;
+    //[SerializeField] GameObject ChunkTwo;
+    //[SerializeField] GameObject ChunkThree;
+    //[SerializeField] GameObject ChunkFour;
+    //[SerializeField] GameObject ChunkFive;
+    //[SerializeField] GameObject ChunkSix;
+
+
+    // Use this for initialization
+    void Awake () {
+		if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+       LoadScene(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadScene(int index)
+    {
+        SceneManager.LoadSceneAsync(index, LoadSceneMode.Additive);
+    }
+
+    void UnloadScene(Scene toUnload)
+    {
+        if (toUnload.name != "Main")
+            StartCoroutine(UnloadSceneCoruitine(toUnload));
+
+    }
+
+    IEnumerator UnloadSceneCoruitine(Scene sceneToUnload)
+    {
+        while (!sceneToUnload.isLoaded)
+        {
+            yield return null;
+        }
+        SceneManager.UnloadSceneAsync(sceneToUnload.buildIndex);
+    }
+
+    public void CreateNewChunk(List<GameObject> Platforms)
+    {
+        UnityEngine.SceneManagement.Scene rootScene = Platforms[0].scene;
+        GameObject newChunk = new GameObject();
+
+        foreach(GameObject a in Platforms)
+        {
+            a.transform.SetParent(newChunk.transform, false);
+            a.transform.SetAsLastSibling();
+        }
+        UnloadScene(rootScene);
+    }
+}
---
+++ Mete/Assets/ChunkScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        List<GameObject> children = new List<GameObject>();
+      for(int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            children.Add(transform.GetChild(i).gameObject);
+        }
+        ChunkLoading.Instance.CreateNewChunk(children);
+        Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
