1dcecef8be99fd35d4c6df90762d4b729436e23c
Aria Myers (RIT Student) <jsm3767@ad.rit.edu>
2018-04-10 14:31:12 -0400

    Game Manager Prefab Stuff Works Nice

--- Mete/Assets/Scripts/GameManager.cs
+++ Mete/Assets/Scripts/GameManager.cs
@@ -7,7 +7,9 @@ public class GameManager : MonoBehaviour {
     float songBPM;
     float secondsPerBeat;
 
-    GameObject Chunk;
+    [SerializeField] GameObject Chunk;
+
+    Queue<GameObject> chunks;
 
     float timer;
     private int shake;
@@ -17,6 +19,12 @@ public class GameManager : MonoBehaviour {
         timer = 0;
         songBPM = 60;
         secondsPerBeat = 60.0f / songBPM;
+
+        chunks = new Queue<GameObject>();
+        Pulse();
+        Pulse();
+        Pulse();
+        Pulse();
 	}
 	
 	// Update is called once per frame
@@ -32,6 +40,19 @@ public class GameManager : MonoBehaviour {
 
     void Pulse()
     {
+        this.transform.position = this.transform.position + new Vector3(12,0,0);
+        GameObject spawn = Instantiate(Chunk);
+        spawn.transform.position = this.transform.position;
         Debug.Log("Boom");
+
+
+        chunks.Enqueue(spawn);
+        if (chunks.Count > 4)
+        {
+            GameObject fall = chunks.Dequeue();
+            Rigidbody rb = fall.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            Destroy(fall, 10);
+        }
     }
 }
