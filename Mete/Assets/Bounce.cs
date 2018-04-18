a43022cacea54c43a2534a1333ba59ec9e0ee9f8
zanthous <bcm023@gmail.com>
2018-04-18 12:06:22 -0400

    made a couple chunks

---
+++ Mete/Assets/Bounce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float bounciness = 30.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            if(collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, bounciness), ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Mete/Assets/Button.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public List<GameObject> ObjectsToDisable;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("entered");
+
+        if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            foreach (GameObject g in ObjectsToDisable)
+            {
+                Debug.Log("deactivating");
+
+                g.SetActive(false);
+            }
+        }
+    }
+}
