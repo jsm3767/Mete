5855edb26dccea1989351a06e3fbd2a8e733c74c
zanthous <bcm023@gmail.com>
2018-04-26 14:28:09 -0400

    chunk creation stuff

---
+++ Mete/Assets/Fan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(gameObject.transform.position.x,gameObject.transform.position.y), Vector2.up);
+        if(hit.transform.tag == "Player")
+        {
+            Debug.Log("hit");
+            hit.transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1.0f), ForceMode2D.Force);
+        }
+    }
+}
