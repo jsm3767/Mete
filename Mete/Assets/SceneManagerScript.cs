0f32debfab2abff96837374dfbcf864063bdf458
Paul Wilson <pjw6900@rit.edu>
2018-04-27 00:41:24 -0400

    Menu

---
+++ Mete/Assets/SceneManagerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour {
+
+    public void Startgame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
--- Mete/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Mete/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
 
 namespace UnityStandardAssets._2D
 {
@@ -29,6 +30,9 @@ namespace UnityStandardAssets._2D
 
         private void FixedUpdate()
         {
+            //check if out of bounds
+            if (gameObject.transform.position.y < -2)
+                SceneManager.LoadScene("Menu");
             // Read the inputs.
             bool crouch = Input.GetKey(KeyCode.LeftShift);
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
